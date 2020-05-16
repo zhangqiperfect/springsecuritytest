5a825cb1110648a192432471a34f55a78f3c1ed7
“zhangqi <2994182080qq.com>
2020-05-16 10:29:27 +0800

    新增testGit类

---
+++ src/main/java/com/spring/springsecuritytest/service/TestGit.java
@@ -0,0 +1,14 @@
+package com.spring.springsecuritytest.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ZQ
+ * @create 2020-05-16
+ */
+@Component
+public class TestGit {
+    public String testGit() {
+        return "hello git";
+    }
+}
